fa777a76f519c03c68fef1f247eb320dce95e21b
shigemk2 <i.am.shige@gmail.com>
2016-10-22 18:32:15 +0900

    Extend CompletableApp(2)

--- src/main/scala/com/example/GuaranteedDelivery.scala
+++ src/main/scala/com/example/GuaranteedDelivery.scala
@@ -3,7 +3,5 @@ package com.example
 import akka.actor._
 import com.example._
 
-object GuaranteedDeliveryDriver {
-  def main(args: Array[String]): Unit = {
-  }
+object GuaranteedDeliveryDriver extends CompletableApp(2) {
 }
