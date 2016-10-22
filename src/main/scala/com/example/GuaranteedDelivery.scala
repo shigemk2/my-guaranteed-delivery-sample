59385a8d1459ace806668b9758da8311fa58f461
shigemk2 <i.am.shige@gmail.com>
2016-10-22 19:07:11 +0900

    Add OrderProcessor

--- src/main/scala/com/example/GuaranteedDelivery.scala
+++ src/main/scala/com/example/GuaranteedDelivery.scala
@@ -1,5 +1,7 @@
 package com.example
 
+import akka.actor.ActorPath
+import akka.persistence.{Channel, Deliver, PersistenceFailure, Persistent, Processor, Recover, SnapshotSelectionCriteria}
 import com.example._
 
 object GuaranteedDeliveryDriver extends CompletableApp(2) {
@@ -7,3 +9,33 @@ object GuaranteedDeliveryDriver extends CompletableApp(2) {
 
 case class ProcessOrder(orderId: String, details: String)
 
+class OrderProcessor(orderAnalyzer: ActorPath) extends Processor {
+  val channel =
+    context.actorOf(
+      Channel.props(),
+      s"${self.path.name}-channel"
+    )
+
+  override def preStart() = {
+    self ! Recover(replayMax = 0L)
+  }
+
+  def receive = {
+    case message @ Persistent(actualMessage, sequenceNumber) =>
+      print(s"Handling persisted: $sequenceNumber: ")
+      actualMessage match {
+        case processOrder: ProcessOrder =>
+          println(s"ProcessOrder: $processOrder")
+          channel ! Deliver(message, orderAnalyzer)
+          GuaranteedDeliveryDriver.completedStep
+        case unknown: Any =>
+          println(s"Unknown: $unknown")
+      }
+    case PersistenceFailure(actualMessage, sequenceNumber, cause) =>
+      println(s"Handling failed persistent: acutalMessage")
+      GuaranteedDeliveryDriver.completedStep
+    case non_persisted: Any =>
+      println(s"Handling non-persistent: $non_persisted")
+      GuaranteedDeliveryDriver.completedStep
+  }
+}
